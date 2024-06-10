ad8e0a63dbb0b5cc0f0c896b44512db08ae29c8d
YIS <dtdt203@naver.com>
2024-06-10 15:36:06 +0900

    미완성이요..

--- src/redux/slices/calculatorSlice.js
+++ src/redux/slices/calculatorSlice.js
@@ -1,1 +1,18 @@
 // TODO: result 상태와 덧셈, 뺄셈에 대한 상태변경 로직을 담은 slice를 만들어 보세요.
+import { createSlice } from "@reduxjs/toolkit";
+
+const calculatorSlice = createSlice({
+  name: "calculator",
+  initialState: 0,
+  reducers: {
+    add: (state, action) => {
+      state += action.payload;
+    },
+    subtract: (state, action) => {
+      state -= action.payload;
+    },
+  },
+});
+
+export const { add, subtract } = calculatorSlice.actions;
+export default calculatorSlice.reducer;
