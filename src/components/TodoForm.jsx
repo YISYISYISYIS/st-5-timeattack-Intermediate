f765492618cc4ea8f6c246f5d7cb2e155075cbb2
YIS <dtdt203@naver.com>
2024-06-10 16:38:01 +0900

    죄송합니다... 폼깔짝하다 끝났어요

--- src/components/TodoForm.jsx
+++ src/components/TodoForm.jsx
@@ -1,8 +1,23 @@
+import { useSelector, useDispatch } from "react-redux";
 export default function TodoForm() {
+  const todos = useSelector((state) => state.todos);
+  const dispatch = useDispatch();
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+
+    const newTodos = {
+      id: crypto.randomUUID(),
+      title,
+      content,
+      isDone: false,
+    };
+    dispatch(addTodo(newTodos));
+  };
   return (
-    <form>
+    <form onSubmit={onSubmit}>
       <label>제목: </label>
-      <input type="text" /> <label>내용: </label>
+      <input type="text" value={title} /> <label>내용: </label>
       <input type="text" />
       <button>추가</button>
     </form>
--- src/redux/slices/todoSlice.js
+++ src/redux/slices/todoSlice.js
@@ -1,1 +1,34 @@
 // TODO: todoSlice 를 작성하세요.
+import { createSlice } from "@reduxjs/toolkit";
+
+const todosSlice = createSlice({
+  name: "todos",
+  initialState: [
+    {
+      id: 1,
+      title: "리액트1",
+      content: "내용입니다1",
+      isDone: false,
+    },
+    {
+      id: 2,
+      title: "리액트2",
+      content: "내용입니다2",
+      isDone: true,
+    },
+  ],
+  reducers: {
+    workingTodos: (state, action) => {
+      state.filter((state) => !state.isDone);
+    },
+    doneTodos: (state, action) => {
+      state.filter((state) => state.isDone);
+    },
+    addTodo: (state, action) => {
+      state.push(action.payload);
+    },
+  },
+});
+
+export const { workingTodos, doneTodos } = todosSlice.actions;
+export default todosSlice.reducer;
